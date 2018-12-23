9e7e8fb69d8d627e7453933b6d71052e8619c1e9
Yoav Hizkiahou <yoav.hizkiahou@aquasec.com>
2018-12-23 11:43:09 +0200

    Added cobra CLI to the project, made it Runnable.
    Support the attributes:
    
    config - Given full path to a yaml configuration file.
    json - Print results in json format.
    noRemediations - Disable printing remediation.

---
+++ app.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aquasecurity/bench-common/check"
+	"github.com/aquasecurity/bench-common/util"
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"os"
+)
+
+func app(cmd *cobra.Command, args []string) {
+	err := checkDefinitionFilePath(cfgFile)
+	if err != nil {
+		util.ExitWithError(err)
+	}
+
+	controls, err := getControls(cfgFile)
+	if err != nil {
+		util.ExitWithError(err)
+	}
+
+	summary := runControls(controls, "")
+	err = outputResults(controls, summary)
+	if err != nil {
+		util.ExitWithError(err)
+	}
+}
+
+func outputResults(controls *check.Controls, summary check.Summary) error {
+	// if we successfully ran some tests and it's json format, ignore the warnings
+	if (summary.Fail > 0 || summary.Warn > 0 || summary.Pass > 0) && jsonFmt {
+		out, err := controls.JSON()
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(out))
+	} else {
+		util.PrettyPrint(controls, summary, noRemediations)
+	}
+
+	return nil
+}
+
+func runControls(controls *check.Controls, checkList string) check.Summary {
+	var summary check.Summary
+
+	if checkList != "" {
+		ids := util.CleanIDs(checkList)
+		summary = controls.RunChecks(ids...)
+	} else {
+		summary = controls.RunGroup()
+	}
+
+	return summary
+}
+
+func getControls(path string) (*check.Controls, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	controls, err := check.NewControls([]byte(data))
+	if err != nil {
+		return nil, err
+	}
+
+	return controls, err
+}
+
+func checkDefinitionFilePath(filePath string) (err error) {
+	glog.V(2).Info(fmt.Sprintf("Looking for config file: %s\n", filePath))
+	_, err = os.Stat(filePath)
+
+	return err
+}
