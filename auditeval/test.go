ac1bd9ce21f7ae80591ddfa01ba198eae2edde5d
yoavrotems <yoavrotems97@gmail.com>
2019-05-07 17:55:52 +0300

    Add option to flag
    Now "op: eq" will catch "flag=value" and "flag = value"

--- auditeval/test.go
+++ auditeval/test.go
@@ -154,6 +154,7 @@ func getFlagValue(s, flag string) string {
 
 	var flagVal string
 	pttns := []string{
+		flag + ` = ([^ \n]*)`,
 		flag + `=([^ \n]*)`,
 		flag + ` +([^- ]+)`,
 		`(?:^| +)` + `(` + flag + `)` + `(?: |$)`,
