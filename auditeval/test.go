c75f5a3c0888397684fbf04e28d2cb01c461e4b4
yoavrotems <yoavrotems97@gmail.com>
2019-05-21 15:16:27 +0300

    Update test.go

--- auditeval/test.go
+++ auditeval/test.go
@@ -154,7 +154,7 @@ func getFlagValue(s, flag string) string {
 
 	var flagVal string
 	pttns := []string{
-		flag + `\s*=\s*([^ \n]*`,
+		flag + `\s*=\s*([^ \n]*)`,
 		flag + `=([^ \n]*)`,
 		flag + ` +([^- ]+)`,
 		`(?:^| +)` + `(` + flag + `)` + `(?: |$)`,
