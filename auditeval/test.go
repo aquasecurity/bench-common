d9f6678b0311e0207a33111d8afde20e671f3486
yoavrotems <yoavrotems97@gmail.com>
2019-11-03 16:14:07 +0200

    Update test.go
    More tests

--- auditeval/test.go
+++ auditeval/test.go
@@ -158,10 +158,10 @@ func getFlagValue(s, flag string) string {
 
 	var flagVal string
 	pttns := []string{
-		flag + `\s*[=:][\r\t\f\v ]*"(.*)"`,
-		flag + `\s*[=:][\r\t\f\v ]*([^\s]*)`,
-		flag + `\s+([^-\s]+)`,
-		`(?:^| +)` + `(` + flag + `)` + `(?: |$)`,
+		`(?:^|[\s]+)` + flag + `\s*[=:][\r\t\f\v ]*"(.*)"`,
+		`(?:^|[\s]+)` + flag + `\s*[=:][\r\t\f\v ]*([^\s]*)`,
+		`(?:^|[\s]+)` + flag + `\s+([^-\s]+)`,
+		`(?:^|[\s]+)` + `(` + flag + `)` + `(?:[\s]|$)`,
 	}
 	for _, pttn := range pttns {
 		flagRe := regexp.MustCompile(pttn)
