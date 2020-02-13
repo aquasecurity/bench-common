1cadaf7d1c89728e68ac08f9ed092d517aa1a5ff
Roberto J Rojas <robertojrojas@gmail.com>
2020-02-13 11:00:46 -0500

    changes as per PR review

--- outputter/writer.go
+++ outputter/writer.go
@@ -7,36 +7,27 @@ import (
 	"os"
 )
 
-var errMissingIOWriter = fmt.Errorf("IOWriter is required")
-
 type fileHandler interface {
 	Handle(data string) error
 }
 
 type file struct {
 	Filename string
-	ioWriter ioWriter
 }
 
 type ioWriter interface {
 	OutputToWriter(data string, w io.Writer) error
 }
 
-type ioWriteDelegate struct{}
-
 func newFile(filename string) *file {
 	return &file{
 		Filename: filename,
-		ioWriter: &ioWriteDelegate{},
 	}
 }
 
 func (f *file) Handle(data string) error {
-	if err := f.validate(); err != nil {
-		return err
-	}
-
 	var w io.Writer
+	var err error
 	if len(f.Filename) == 0 {
 		w = os.Stdout
 	} else {
@@ -50,19 +41,7 @@ func (f *file) Handle(data string) error {
 
 	writer := bufio.NewWriter(w)
 	defer writer.Flush()
+	_, err = fmt.Fprintln(w, data)
 
-	return f.ioWriter.OutputToWriter(data, w)
-}
-
-func (f *file) validate() error {
-	if f.ioWriter == nil {
-		return errMissingIOWriter
-	}
-
-	return nil
-}
-
-func (iowd *ioWriteDelegate) OutputToWriter(data string, w io.Writer) error {
-	_, err := fmt.Fprintln(w, data)
 	return err
 }
