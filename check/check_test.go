fac86a7113101a559cac3b4ab850693c8ab5fa5c
yoavrotems <yoavrotems97@gmail.com>
2020-04-16 22:42:54 +0300

    Update check_test.go

--- check/check_test.go
+++ check/check_test.go
@@ -1,6 +1,7 @@
 package check
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/aquasecurity/bench-common/auditeval"
@@ -191,4 +192,71 @@ func TestGetFirstValidSubCheck(t *testing.T) {
 			}
 		}
 	}
+}
+func TestTextToCommand(t *testing.T) {
+	type TestCase struct {
+		auditCommand          string
+		expectedSlicedCommand []string
+	}
+
+	// For each test case, we want get the different commands separated by | and exclude the \\|
+	// which stand for the use of | as OR operator and not as a pipe.
+	testCases := []TestCase{
+		{
+			// Test for mixed case of pipeline and or cases
+			auditCommand: "grep -E -i \"(\\\\v\\|\\\\r\\|\\\\m\\|\\\\s)\\|$(grep '^ID=' /etc/os-release | cut -d= -f2 | sed -e 's/\"//g'))\" /etc/motd",
+			expectedSlicedCommand: []string{
+				"grep -E -i \"(\\\\v|\\\\r|\\\\m|\\\\s)|$(grep '^ID=' /etc/os-release",
+				"cut -d= -f2",
+				"sed -e 's/\"//g'))\" /etc/motd"},
+		},
+		{
+			// Test for regular pipe
+			auditCommand: "lsmod | grep cramfs",
+			expectedSlicedCommand: []string{
+				"lsmod",
+				"grep cramfs"},
+		},
+		{
+			// Test for | as Or
+			auditCommand: "grep -E \"^(server\\|pool)\" /etc/ntp.conf",
+			expectedSlicedCommand: []string{
+				"grep -E \"^(server|pool)\" /etc/ntp.conf"},
+		},
+	}
+	for i, testCase := range testCases {
+		commands := textToCommand(testCase.auditCommand)
+		for j, command := range commands {
+			testSlicedCommand := strings.Join(command.Args[:], " ")
+			if testSlicedCommand != testCase.expectedSlicedCommand[j] {
+				t.Errorf("case %d expected:%v, got:%v\n", i, testCase.expectedSlicedCommand[j], testSlicedCommand)
+			}
+		}
+	}
+}
+func TestIsShellCommand(t *testing.T) {
+	type TestCase struct {
+		command  string
+		Expected bool
+	}
+
+	// For each test case, we want to find the first subcheck that matches the constraints in testDefinedConstraints
+	testCases := []TestCase{
+		{
+			// Exist command
+			command:  "/bin/grep",
+			Expected: true,
+		},
+		{
+			// Non exist command
+			command:  "/bin/nonExistCommand",
+			Expected: false,
+		},
+	}
+	for ii, testCase := range testCases {
+		if isShellCommand(testCase.command) != testCase.Expected {
+			t.Errorf("case %d expected for isShellCommand(\"%v\") to return: %v\n", ii, testCase.command, testCase.Expected)
+		}
+	}
+
 }
